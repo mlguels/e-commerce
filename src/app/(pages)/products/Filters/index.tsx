acbec003e31f2e5c1d3a4068f6c52ebab358c2ae
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2023-12-14 22:15:35 -0800

    Implemented Filter categories & sort radio buttons

--- src/app/(pages)/products/Filters/index.tsx
+++ src/app/(pages)/products/Filters/index.tsx
@@ -1,9 +1,61 @@
+'use client'
+
 import React from 'react'
+
+import { Category } from '../../../../payload/payload-types'
+import { Checkbox } from '../../../_components/Checkbox'
+import { HR } from '../../../_components/HR'
+import { RadioButton } from '../../../_components/Radio'
+import { useFilter } from '../../../_providers/Filter'
 
 import classes from './index.module.scss'
 
-const Filters = () => {
-  return <div>Filters</div>
+const Filters = ({ categories }: { categories: Category[] }) => {
+  const { categoryFilters, sort, setCategoryFilters, setSort } = useFilter()
+
+  const handleCategories = (categoryId: string) => {}
+  const handleSort = () => {}
+
+  return (
+    <div className={classes.filter}>
+      <div className={classes.title}>
+        <h6>Product Categories</h6>
+        <div>
+          {categories.map(category => {
+            const isSelected = categoryFilters.includes(category.id)
+
+            return (
+              <Checkbox
+                key={category.id}
+                label={category.title}
+                value={category.id}
+                isSelected={isSelected}
+                onClickHandler={handleCategories}
+              />
+            )
+          })}
+        </div>
+        <HR className={classes.hr} />
+        <h6 className={classes.title}>Sort by</h6>
+        <div className={classes.categories}>
+          <RadioButton
+            label="Latest"
+            value="-createdAt"
+            isSelected={sort === '-createdAt'}
+            onRadioChange={handleSort}
+            groupName="sort"
+          />
+          <RadioButton
+            label="Oldest"
+            value="-createdAt"
+            isSelected={sort === '-createdAt'}
+            onRadioChange={handleSort}
+            groupName="sort"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Filters
